2173f6a021831a73e727ccb320c495ab30a81b68
laloparkour <laloparkour@gmail.com>
2022-08-24 16:56:38 -0600

    Ejercicio 22

--- APIIDSTV/14 - requestAnimationFrame/index.php
+++ APIIDSTV/14 - requestAnimationFrame/index.php
@@ -15,12 +15,17 @@
         var cv = null;
         var ctx = null;
         var player1 = null;
+        var player2 = null;
         var super_x = 240, super_y = 240;
+        
+        var direction = 'left';
+        var score = 0;
 
         function start () {
             cv = document.getElementById("mycanvas");
             ctx = cv.getContext('2d');
             player1 = new Cuadrado(super_x, super_y, 40, 40, "red");
+            player2 = new Cuadrado(generateRandomInteger(500), generateRandomInteger(500), 40, 40, "red");
             paint();
         }
 
@@ -30,12 +35,16 @@
 
             ctx.fillStyle = "white";
             ctx.fillRect(0, 0, 500, 500);
+
+            ctx.fillText('SCORE:' + score, 30, 30);
+            
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, 500, 500);
+
             
             player1.c = random_rgba();
             player1.dibujar(ctx);
-/*             ctx.fillStyle = random_rgba();
-            ctx.fillRect(super_x, super_y, 40, 40);
-            ctx.strokeRect(super_x, super_y, 40, 40); */
+            player2.dibujar(ctx);
 
             update();
 
@@ -48,6 +57,16 @@
             this.h = h;
             this.c = c;
 
+            this.se_tocan = function (target) { 
+
+                if(this.x < target.x + target.w &&
+                this.x + this.w > target.x && 
+                this.y < target.y + target.h && 
+                this.y + this.h > target.y) {
+                    return true;  
+                }  
+            };
+
             this.dibujar = function(ctx) {
                 ctx.fillStyle = this.c;
                 ctx.fillRect(this.x, this.y, this.w, this.h);
@@ -57,17 +76,77 @@
         }
 
         function update() {
-            player1.x += 10; 
-            if (player1.x > 500) {
-                player1.x = 0;
+
+            if (direction == 'right') {
+                player1.x += 10; 
+                if (player1.x > 500) {
+                    player1.x = 0;
+                }
+
+            } 
+
+            if (direction == 'left') {
+                player1.x -= 10; 
+                if (player1.x < 0) {
+                    player1.x = 500;
+                }
+
             }
 
-            player1.y += 10; 
-            if (player1.y > 500) {
-                player1.y = 0;
+            if (direction == 'down') {
+                player1.y += 10; 
+                if (player1.y > 500) {
+                    player1.y = 0;
+                }
+
+            }
+
+            if (direction == 'up') {
+                player1.y -= 10; 
+                if (player1.y < 0) {
+                    player1.y = 500;
+                }
+
+            }
+
+            if (player1.se_tocan(player2)) {
+                player2.x = generateRandomInteger(500);
+                player2.y = generateRandomInteger(500);
+
+                score += 10;
+
             }
 
 
+        }
+
+        document.addEventListener('keydown', (e) => {
+            console.log(e);
+
+            // Arriba
+            if (e.keyCode == 87 || e.keyCode == 38) {
+                direction = 'up';
+            }
+            
+            // Abajo
+            if (e.keyCode == 83 || e.keyCode == 40) {
+                direction = 'down';
+            }
+            
+            // Izquierda
+            if (e.keyCode == 65 || e.keyCode == 37) {
+                direction = 'left';
+            }
+            
+            // Derecha
+            if (e.keyCode == 68 || e.keyCode == 39) {
+                direction = 'right';
+            }
+            
+        });
+
+        function generateRandomInteger(max) {
+            return Math.floor(Math.random() * max) + 1;
         }
 
         window.addEventListener('load', start);
