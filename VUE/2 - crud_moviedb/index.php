2903c37aa5ab1497dd2477d6737d0ac0d46f65fe
laloparkour <laloparkour09@gmail.com>
2022-09-14 15:56:36 -0600

    Ejercicio 12 (falto desvalorar)

--- VUE/2 - crud_moviedb/index.php
+++ VUE/2 - crud_moviedb/index.php
@@ -5,27 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vue</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     
 </head>
-<body>
-
+<body class="m-0 vh-100 d-flex justify-content-center align-items-center">
     <div id="contenedor">
-        <form action="">
-            <fieldset>
-                <legend>
-                    Datos de acceso
-                </legend>
-                <label>Correo electronico</label>
-                <input type="text" v-model="email">
-                <br>
-                <label>Password</label>
-                <input type="password" v-model="password">
-                <br>
-                <button @click="login">
-                    Acceder
-                </button>
-            </fieldset>
-        </form>
+        <div class="row">
+            <div class="col p-5 text-center card">
+                <form action="" class="">
+                    <h1 class="h3 mb-3 fw-normal">
+                        Iniciar sesión
+                    </h1>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" v-model="email" placeholder="Correo electrónico">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" v-model="password" placeholder="Contraseña">
+                    </div>
+                    <div class="">
+                        <button class="w-100 btn btn-lg btn-primary" @click="login">
+                            Acceder
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <script src="https://unpkg.com/vue@3"></script>
@@ -64,7 +69,7 @@
                     .then(function (response) {
                         if (response.data.success) {
                             localStorage.setItem('peliculas', JSON.stringify(response));
-                            window.location.href = 'users.html';
+                            window.location.href = 'movies.html';
 
                         } else {
                             alert("Datos incorrectos");
