f181b55cb88c343255d4c32e88ec76e2bba4a458
laloparkour <laloparkour@gmail.com>
2022-09-15 18:01:55 -0600

    Se creo una nueva carpeta de unidad

---
+++ NODE, LESS/0 - install less/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Panel</title>
+</head>
+<body>
+    
+    <div class="contaner">
+
+        <fieldset>            
+            <legend>Acceso al panel</legend>
+            <form action="">                
+    
+                <label for="">Email</label>
+                <input type="Email">
+    
+                <label for="">Contraseña</label>
+                <input type="password">
+    
+                <button type="submit">Acceder</button>
+            </form>    
+        </fieldset>
+    </div>
+
+
+</body>
+</html>
