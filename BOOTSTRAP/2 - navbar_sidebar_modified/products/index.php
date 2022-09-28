ad94b443943273433d6b70949ad08c14a06a41d2
laloparkour <laloparkour09@gmail.com>
2022-09-28 15:46:32 -0600

    productos

--- BOOTSTRAP/2 - navbar_sidebar_modified/products/index.php
+++ BOOTSTRAP/2 - navbar_sidebar_modified/products/index.php
@@ -81,35 +81,61 @@
 
 				</div>
 
-				<div class="col">
+				<div class="col-md-10 col-lg-10 col-sm-12">
 					
+					<section>
+						
+						<div class="row bg-light m-2">
+							
+							<div class="col">
+								<nav aria-label="breadcrumb">
+									<ol class="breadcrumb">
+										<li class="breadcrumb-item"><a href="#">Home</a></li>
+										<li class="breadcrumb-item active" aria-current="page">Products</li>
+									</ol>
+									<div class="col float-end">
+										<button  data-bs-toggle="modal" data-bs-target="#addProductModal">Añadir botón</button>
+									</div>
+								</nav>                            
+							</div>
+
+						</div>
+
+					</section> 
+
 					<section>
 						
 						<div class="row">
 							
-							<div class="col-12">
-								
-                                
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Products</li>
-                                </ol>
-                                <div class="col float-end">
-                                    <button  data-bs-toggle="modal" data-bs-target="#addProductModal">Añadir botón</button>
-                                </div>
-                            </nav>
+							<?php for ($i=0; $i < 12; $i++): ?>
 
-                                <div class="card" style="width: 18rem;">
-                                    <img src="..." class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            
+							<div class="col-md-4 col-sm-12"> 
+
+								<div class="card mb-2">
+								  <img src="../public/img/logo.png" class="card-img-top" alt="...">
+								  <div class="card-body">
+								    <h5 class="card-title">Card title</h5>
+								    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+								    <div class="row">
+									    <a data-bs-toggle="modal" data-bs-target="#addProductModal" href="#" class="btn btn-warning mb-1 col-6">
+									    	Editar
+									    </a>
+									    <a onclick="eliminar(this)" href="#" class="btn btn-danger mb-1 col-6">
+									    	Eliminar
+									    </a>
+									    <a href="#" class="btn btn-info col-12">
+									    	Detalles
+									    </a>
+								    </div>
+
+								  </div>
+								</div>  
+
 							</div>
+
+							<?php endfor; ?>
 
 						</div>
 
@@ -124,30 +150,44 @@
 
 
         <!-- Modal -->
+<!-- Modal -->
 <div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Añadir producto</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-            <form action="">
-            <div class="modal-body">
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">@</span>
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-            </form>
-    </div>
-  </div>
-</div>
+		  <div class="modal-dialog">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		      </div>
 
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+		      <form>
+
+			      <div class="modal-body">
+			        
+			        <?php for ($i=0; $i < 6; $i++): ?>
+			        <div class="input-group mb-3">
+					  <span class="input-group-text" id="basic-addon1">@</span>
+					  <input required type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+					</div>
+					<?php endfor; ?>
+
+			      </div>
+
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+			        	Close
+			        </button>
+			        <button type="submit" class="btn btn-primary">
+			        	Save changes
+			        </button>
+			      </div>
+
+		      </form>
+
+		    </div>
+		  </div>
+		</div>
+
+<!-- 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
         <script>
             eliminar() {
                 Swal.fire({
@@ -168,6 +208,29 @@
                     }
                 })
             }
-        </script>
+        </script> -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+		<script type="text/javascript">
+			function eliminar(target)
+			{
+				swal({
+				  title: "Are you sure?",
+				  text: "Once deleted, you will not be able to recover this imaginary file!",
+				  icon: "warning",
+				  buttons: true,
+				  dangerMode: true,
+				})
+				.then((willDelete) => {
+				  if (willDelete) {
+				    swal("Poof! Your imaginary file has been deleted!", {
+				      icon: "success",
+				    });
+				  } else {
+				    swal("Your imaginary file is safe!");
+				  }
+				});
+			}
+		</script>
 	</body>
 </html>
